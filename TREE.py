94f2299e4104a93432c99317d92e7a6c095f5b7d
Deep Contractor <31130563+chesterking123@users.noreply.github.com>
2020-10-19 22:13:50 +0530

    Update TREE.py

--- TREE.py
+++ TREE.py
@@ -232,3 +232,29 @@ def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
     print(check)
     if check[0]==check[2] and check[1]!=check[3]:
         return True
+
+   
+
+def findLCA(root, n1, n2): 
+    if root is None: 
+        return None
+  
+    # If either n1 or n2 matches with root's key, report 
+    #  the presence by returning root (Note that if a key is 
+    #  ancestor of other, then the ancestor key becomes LCA 
+    if root.data == n1 or root.data == n2: 
+        return root  
+  
+    # Look for keys in left and right subtrees 
+    left_lca = findLCA(root.left, n1, n2)  
+    right_lca = findLCA(root.right, n1, n2) 
+  
+    # If both of the above calls return Non-NULL, then one key 
+    # is present in once subtree and other is present in other, 
+    # So this node is the LCA 
+    if left_lca and right_lca: 
+        return root  
+  
+    # Otherwise check if left subtree or right subtree is LCA 
+    return left_lca if left_lca is not None else right_lca 
+  
