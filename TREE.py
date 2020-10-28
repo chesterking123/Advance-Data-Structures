e962df316cf7c3988f5490cc5fdfe052e582d57a
Deep Contractor <31130563+chesterking123@users.noreply.github.com>
2020-10-28 10:26:06 +0530

    Update TREE.py

--- TREE.py
+++ TREE.py
@@ -256,6 +256,32 @@ def findLCA(root, n1, n2):
         return left_lca
     else:
         return right_lca
+    
+    #
+    #
+    #
+    #program for maximum sum problem without father son relationship
+    def maxSumHelper(root) : 
+  
+    if (root == None):  
+      
+        sum = [0, 0]  
+        return sum
+      
+    sum1 = maxSumHelper(root.left)  
+    sum2 = maxSumHelper(root.right)  
+    sum = [0, 0] 
+  
+    # This node is included (Left and right  
+    # children are not included)  
+    sum[0] = sum1[1] + sum2[1] + root.data  
+  
+    # This node is excluded (Either left or  
+    # right child is included)  
+    sum[1] = (max(sum1[0], sum1[1]) + 
+              max(sum2[0], sum2[1]))  
+  
+    return sum
   
 
 def printkDistanceNodeDown(root, k): 
