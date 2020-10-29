8c62d2367f124135a111a811af316172a7726491
Deep Contractor <31130563+chesterking123@users.noreply.github.com>
2020-10-29 12:41:32 +0530

    Update Lvlorder*.py

--- Lvlorder*.py
+++ Lvlorder*.py
@@ -15,3 +15,27 @@ def printLevelOrder(root):
             count -= 1
 
         print(' ') 
+
+
+def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+    lvl=0
+    check=[]
+    node=root
+    def check_cousin(root,lvl,check,node):
+        #print(a,c,root)
+        if len(check)==4 or root==None:
+            return
+        if root.val==x:
+            check.append(lvl)
+            check.append(node.val)
+        if root.val==y:
+            check.append(lvl)
+            check.append(node.val)
+        lvl+=1
+        m=root
+        check_cousin(root.left,lvl,check,node)
+        check_cousin(root.right,lvl,check,node)
+    check_cousin(root,lvl,check,node)
+    print(check)
+    if check[0]==check[2] and check[1]!=check[3]:
+        return True
